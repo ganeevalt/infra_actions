fac4737b89794c48a396f80d325a7c6a4923aeb7
unknown <ganeevalt@yandex.ru>
2023-03-13 09:45:51 +0400

    Tenth commit

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'infra_app'
+app_name = "infra_app"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('second_page/', views.second_page, name='second_page'),
+    path("", views.index, name="index"),
+    path("second_page/", views.second_page, name="second_page"),
 ]
